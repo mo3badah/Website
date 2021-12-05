258ead57b0faa7477d9f0a8a3b77068b6f03f1d7
Mohamed Reda <alprofmohamed123@gmail.com>
2021-12-05 21:56:50 +0200

    Adding the checking the message before send it.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,32 @@ darkmode.onclick = () => {
         document.body.classList.remove('active');
     }
 }
+// Validation of the data to be sent as an email
+// 1: get the main fields.
+let userName = document.getElementById('name');
+let userMail = document.getElementById('mail');
+let userText = document.getElementById('text');
+// 2: Made the function to filter.
+console.log(document.forms[0]);
+document.forms[0].onsubmit = function (e){
+    let nameValid = false;
+    let mailValid = false;
+    let textValid = false;
+
+    //    2.1: Check the name is less than 30 characters and not empty filed.
+    if (userName.value !== "" && userName.value.length <= 30){
+        nameValid = true;
+    }
+    //    2.2: Check the mail not empty filed.
+    if (userMail.value !== ""){
+        mailValid = true;
+    }
+    //    2.3: Check the text message not empty filed.
+    if (userText.value !== ""){
+        textValid = true;
+    }
+    if (!nameValid || !mailValid || !textValid){
+        e.preventDefault();
+    }
+}
+
